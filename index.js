50f63da6c1ace8a8c5847d9bbe48c5af6670e961
convivial-system-4430 <cdneal091@gmail.com>
2018-12-22 04:23:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ function sayHiToGrandma(string)
 {
   if (string.toUpperCase() === string) return "YES INDEED!";
   if (string.toLowerCase() === string) return "I can\'t hear you!";
-  else return "I love you, too";
+  else return "I love you, too.";
 }
